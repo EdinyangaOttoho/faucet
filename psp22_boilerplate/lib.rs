df26d0f5d1011c875be83e9064f468e13f101473
Edinyanga O. Ottoho <elzucky@gmail.com>
2023-01-15 12:11:43 +0100

    updated repo

--- psp22_boilerplate/lib.rs
+++ psp22_boilerplate/lib.rs
@@ -2,7 +2,7 @@
 #![feature(min_specialization)]
 
 #[openbrush::contract]
-pub mod andromeda_token {
+pub mod psp22_boilerplate {
     use openbrush::traits::String;
     use ink::env::call::FromAccountId;
     
@@ -13,25 +13,25 @@ pub mod andromeda_token {
 
     #[ink(storage)]
     #[derive(Default, Storage)]
-    pub struct AndromedaToken {
+    pub struct Psp22Boilerplate {
         #[storage_field]
         psp22: psp22::Data,
         #[storage_field]
         metadata: metadata::Data,
     }
 
-    impl PSP22 for AndromedaToken {}
+    impl PSP22 for Psp22Boilerplate {}
 
-    impl PSP22Metadata for AndromedaToken {}
+    impl PSP22Metadata for Psp22Boilerplate {}
 
-    impl AndromedaToken {
+    impl Psp22Boilerplate {
         #[ink(constructor)]
-        pub fn new(total_supply:Balance) -> Self {
+        pub fn new(total_supply:Balance, name:String, symbol:String, decimals:u8) -> Self {
             let mut instance = Self::default();
 
-            instance.metadata.name = Some(String::from("AndromedaSwap"));
-            instance.metadata.symbol = Some(String::from("ANS"));
-            instance.metadata.decimals = 12;
+            instance.metadata.name = Some(String::from(name));
+            instance.metadata.symbol = Some(String::from(symbol));
+            instance.metadata.decimals = decimals;
             instance
                 ._mint_to(instance.env().caller(), total_supply)
                 .expect("Should mint total_supply");
